1d534f2ff6e3e227ba030b81fd808ace4ef50ca7
₦Ø₮₳Đ₳₥ <52224580+miraclemenikelechi@users.noreply.github.com>
2023-06-27 14:01:04 +0100

    mobile view

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 import './style.scss';
+
+const
+    // DOM elements
+    menuEl = document.querySelector(".hamburger"),
+    navigationEl = document.querySelector(".navigation"),
+    hamburger = document.querySelector("#menu_icon"),
+
+
+    // menu toggle function
+    toggleMenu = () => {
+        let menuState = navigationEl.classList.value;
+
+        switch (menuState) {
+            case "navigation":
+                navigationEl.classList.add("active");
+                hamburger.src = "icon-close.svg";
+                break;
+
+            case "navigation active":
+                navigationEl.classList.remove("active");
+                hamburger.src = "icon-hamburger.svg";
+                break;
+        }
+    };
+
+menuEl.addEventListener("click", (event) => {
+    toggleMenu();
+});
